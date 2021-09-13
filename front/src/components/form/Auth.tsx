a3796b25ac9bfe3913525e2b47a08f6873fca984
GitHayato <gabayt0_0@icloud.com>
2021-09-13 18:55:39 +0900

    change auth components for signup

--- front/src/components/form/Auth.tsx
+++ front/src/components/form/Auth.tsx
@@ -1,4 +1,4 @@
-import { memo, VFC } from "react";
+import { memo, useState, VFC } from "react";
 import {
   Box,
   Center,
@@ -15,16 +15,26 @@ type Props = {
 
 export const Auth: VFC<Props> = memo((props) => {
   const { title } = props;
+  const [error, setError] = useState("")
+  const passwordConValidation = (values) => {
+    if (values.password !== values.passwordConf) {
+      setError("パスワードとパスワード（確認用）が異なります")
+    }
+  }
 
   return (
     <Formik
       initialValues={{name: "", email: "", password: "", passwordConf: ""}}
       onSubmit={(values) => {
+        if (title === "新規登録") {
+          passwordConValidation(values)
+        }
         console.log(values)
       }}
     >
       {({
         values,
+        errors,
         handleChange,
         handleSubmit
       }) => (
@@ -32,6 +42,15 @@ export const Auth: VFC<Props> = memo((props) => {
             <Center align="center" h="100vh">
               <Box w="335px">
                 <Heading as="h1" fontSize="md">{title}</Heading>
+                {title === "新規登録" && (
+                  <LabelInput
+                    name="name"
+                    value={values.name}
+                    onChange={handleChange}
+                  >
+                    ニックネーム
+                  </LabelInput>
+                )}
                 <LabelInput
                   type="email"
                   name="email"
@@ -48,6 +67,19 @@ export const Auth: VFC<Props> = memo((props) => {
                 >
                   パスワード
                 </LabelInput>
+                {title === "新規登録" && (
+                  <>
+                    <LabelInput
+                      type="password"
+                      name="passwordConf"
+                      value={values.passwordConf}
+                      onChange={handleChange}
+                    >
+                      パスワード（確認用）
+                    </LabelInput>
+                    <Text fontSize="xs" color="red.500">{error}</Text>
+                  </>
+                )}
                 <Box>
                   <BasicButton
                     backgroundColor="#FFA000"
