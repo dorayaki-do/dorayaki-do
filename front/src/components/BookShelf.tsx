8d2740d32ef5c489c3ca4f4e2a599e0b0b86704c
Erina Yamakawa <yamakawa.erina.43z@st.kyoto-u.ac.jp>
2021-09-13 18:29:43 +0900

    fix style

--- front/src/components/BookShelf.tsx
+++ front/src/components/BookShelf.tsx
@@ -1,15 +1,20 @@
-import { Grid, Box, Center, Heading, Stack } from "@chakra-ui/react"
+import { Box, Input, Center, Heading, Stack } from "@chakra-ui/react"
 import { BookGrid } from "./BookGrid"
 
 export const BookShelf = () => {
   return (
-    <Center>
-      <Stack width="80%">
-        <Center>
-          <Heading>本棚</Heading>
-        </Center>
-        <BookGrid></BookGrid>
-      </Stack>
-    </Center>
+    <Box>
+      <Center>
+        <Stack width="80%" spacing={20} mt={20} mb={40}>
+          <Center>
+            <Heading>本棚</Heading>
+          </Center>
+          <Center>
+            <Input width="80%" placeholder="本や作者を検索" />
+          </Center>
+          <BookGrid></BookGrid>
+        </Stack>
+      </Center>
+    </Box>
   )
 }
