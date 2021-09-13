cd8036fa534e5c0c22042cbb653de29c764ae53c
Erina Yamakawa <yamakawa.erina.43z@st.kyoto-u.ac.jp>
2021-09-13 17:48:53 +0900

    create bookshell component

---
+++ front/src/components/BookGrid.tsx
@@ -0,0 +1,37 @@
+import { Box, Grid } from "@chakra-ui/react"
+
+export const BookGrid = () => {
+  return (
+    <Grid
+      templateColumns="repeat(auto-fill, 150px)"
+      justifyContent="center"
+      gap={6}
+      mx="auto"
+    >
+      <Box bg="tomato" width="150px" height="240px"></Box>
+      <Box bg="tomato" width="150px" height="240px"></Box>
+      <Box bg="tomato" width="150px" height="240px"></Box>
+      <Box bg="tomato" width="150px" height="240px"></Box>
+      <Box bg="tomato" width="150px" height="240px"></Box>
+      <Box bg="tomato" width="150px" height="240px"></Box>
+      <Box bg="tomato" width="150px" height="240px"></Box>
+      <Box bg="tomato" width="150px" height="240px"></Box>
+      <Box bg="tomato" width="150px" height="240px"></Box>
+      <Box bg="tomato" width="150px" height="240px"></Box>
+      <Box bg="tomato" width="150px" height="240px"></Box>
+      <Box bg="tomato" width="150px" height="240px"></Box>
+      <Box bg="tomato" width="150px" height="240px"></Box>
+      <Box bg="tomato" width="150px" height="240px"></Box>
+      <Box bg="tomato" width="150px" height="240px"></Box>
+      <Box bg="tomato" width="150px" height="240px"></Box>
+      <Box bg="tomato" width="150px" height="240px"></Box>
+      <Box bg="tomato" width="150px" height="240px"></Box>
+      <Box bg="tomato" width="150px" height="240px"></Box>
+      <Box bg="tomato" width="150px" height="240px"></Box>
+      <Box bg="tomato" width="150px" height="240px"></Box>
+      <Box bg="tomato" width="150px" height="240px"></Box>
+      <Box bg="tomato" width="150px" height="240px"></Box>
+      <Box bg="tomato" width="150px" height="240px"></Box>
+    </Grid>
+  )
+}
--- front/src/components/BookShelf.tsx
+++ front/src/components/BookShelf.tsx
@@ -1,3 +1,15 @@
+import { Grid, Box, Center, Heading, Stack } from "@chakra-ui/react"
+import { BookGrid } from "./BookGrid"
+
 export const BookShelf = () => {
-  return <div>本棚</div>
+  return (
+    <Center>
+      <Stack width="80%">
+        <Center>
+          <Heading>本棚</Heading>
+        </Center>
+        <BookGrid></BookGrid>
+      </Stack>
+    </Center>
+  )
 }
