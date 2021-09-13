900eeca7afd622791970c784e3f7a3ecf04aa6ff
Erina Yamakawa <yamakawa.erina.43z@st.kyoto-u.ac.jp>
2021-09-13 16:40:59 +0900

    fix id error

--- front/src/pages/read/[id].tsx
+++ front/src/pages/read/[id].tsx
@@ -1,12 +1,13 @@
 import React from "react"
 import dynamic from "next/dynamic"
 import { useRouter } from "next/router"
+import { getAsStringFromArray } from "../../utils/getAsStringFromArray"
 
 const Reader = dynamic(() => import("../../components/Reader"), { ssr: false })
 
 const App = () => {
   const router = useRouter()
-  const { id } = router.query
+  const id = getAsStringFromArray(router.query.id)
 
   return <Reader id={id} />
 }
---
+++ front/src/utils/getAsStringFromArray.tsx
@@ -0,0 +1,7 @@
+export function getAsStringFromArray(value: string | string[]): string {
+  if (Array.isArray(value)) {
+    return value[0]
+  }
+
+  return value
+}
