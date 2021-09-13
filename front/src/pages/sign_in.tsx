09f1159f807047f89bf8b68218061eb2a059d1b7
GitHayato <gabayt0_0@icloud.com>
2021-09-13 17:16:03 +0900

    change to components

--- front/src/pages/sign_in.tsx
+++ front/src/pages/sign_in.tsx
@@ -1,63 +1,12 @@
 import { memo, VFC } from "react"
-import {
-  Box,
-  Center,
-  Heading,
-  Text
-} from "@chakra-ui/react"
-import { Form, Formik } from "formik"
 
-import { BasicButton } from "../components/BasicButton"
-import { LabelInput } from "../components/form/LabelInput"
+import { Auth } from "../components/form/Auth"
 
 const SignIn: VFC = memo(() => {
   return (
-    <Formik
-      initialValues={{email: "", password: ""}}
-      onSubmit={(values) => {
-        console.log(values)
-      }}
-    >
-      {({
-        values,
-        handleChange,
-        handleSubmit
-      }) => (
-        <Form onSubmit={handleSubmit}>
-          <Center align="center" h="100vh">
-            <Box w="335px">
-              <Heading as="h1" fontSize="md">ログイン</Heading>
-              <LabelInput
-                type="email"
-                name="email"
-                value={values.email}
-                onChange={handleChange}
-              >
-                メールアドレス
-              </LabelInput>
-              <LabelInput
-                type="password"
-                name="password"
-                value={values.password}
-                onChange={handleChange}
-              >
-                パスワード
-              </LabelInput>
-              <Box>
-                <BasicButton
-                  backgroundColor="#FFA000"
-                  color="#FFFFFF"
-                  type="submit"
-                >
-                  ログイン
-                </BasicButton>
-                <Text fontSize="sm">新規登録</Text>
-              </Box>
-            </Box>
-          </Center>
-        </Form>
-      )}
-    </Formik>
+    <Auth
+      title="ログイン"
+    />
   )
 })
 
