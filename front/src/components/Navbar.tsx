e4b53cfe908cb6b2788f90f34b7b27b13b5446ca
Erina Yamakawa <yamakawa.erina.43z@st.kyoto-u.ac.jp>
2021-09-14 14:05:40 +0900

    arrange nav

--- front/src/components/Navbar.tsx
+++ front/src/components/Navbar.tsx
@@ -1,5 +1,63 @@
-import { Box, Input, Center, Heading, Stack } from "@chakra-ui/react"
+import React from "react"
+import {
+  Box,
+  Button,
+  Center,
+  Text,
+  Link,
+  useDisclosure,
+  Drawer,
+  DrawerBody,
+  DrawerCloseButton,
+  DrawerContent,
+  DrawerOverlay,
+  Flex,
+  IconButton,
+  Spacer,
+  Collapse,
+  VStack,
+  StackDivider,
+} from "@chakra-ui/react"
+import { HamburgerIcon } from "@chakra-ui/icons"
 
 export const Navbar = () => {
-  return <Box>コミなびイベンター</Box>
+  const { isOpen, onToggle } = useDisclosure()
+  const btnRef = React.useRef()
+
+  return (
+    <>
+      <Flex bgColor="#ffa000" minH={50} alignItems="center" px="15%">
+        <Text color="white" fontWeight="bold" fontSize="lg">
+          DorayakiDo
+        </Text>
+        <Spacer />
+        <IconButton
+          aria-label="Menu"
+          icon={<HamburgerIcon />}
+          ref={btnRef}
+          variant="outline"
+          colorScheme="blackAlpha"
+          focusable={true}
+          color="white"
+          onClick={onToggle}
+        />
+      </Flex>
+      <Collapse in={isOpen} animateOpacity={false}>
+        <Box px="15%" py="10px" color="white" bg="#ffa000">
+          <VStack
+            divider={<StackDivider borderColor="white" />}
+            spacing={4}
+            align="stretch"
+          >
+            <Link href="/shelf">
+              <Text fontWeight="bold">本棚</Text>
+            </Link>
+            <Link href="/map">
+              <Text fontWeight="bold">地図</Text>
+            </Link>
+          </VStack>
+        </Box>
+      </Collapse>
+    </>
+  )
 }
