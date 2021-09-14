1bb7f7577efbd279392a34d2fc71b6d34dac5e45
Takuya Shinjo <e175769@ie.u-ryukyu.ac.jp>
2021-09-14 17:19:58 +0900

    Bookテーブルの中身を修正

--- server/pkg/models/book.go
+++ server/pkg/models/book.go
@@ -9,10 +9,11 @@ type Book struct {
 	gorm.Model
 	Title        string `json:"title" binding:"required"`
 	Author       string `json:"author" binding:"required"`
-	Eventname    string `json:"eventname" binding:"required"`
 	Epuburl      string `json:"epuburl" binding:"required"`
 	Thumbnailurl string `json:"thumbnail" binding:"required"`
 	Latitude     string `json:"latitude"`
 	Longitude    string `json:"longitude"`
 	Shopname     string `json:"shopname"`
+	EventID      int    `json:"event_id"`
+	Event        Event
 }
