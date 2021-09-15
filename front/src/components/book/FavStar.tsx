26c80fcfd31f09ea47807a35930b5e1333a5f905
Erina Yamakawa <yamakawa.erina.43z@st.kyoto-u.ac.jp>
2021-09-15 09:49:09 +0900

    add fav button

---
+++ front/src/components/book/FavStar.tsx
@@ -0,0 +1,26 @@
+import { createIcon, StarIcon } from "@chakra-ui/icons"
+
+type Props = {
+  isFav: boolean
+  onClick: () => void
+}
+
+const OutlineStarIcon = createIcon({
+  displayName: "OutlineStarIcon",
+  viewBox: "0 0 24 24",
+  path: (
+    <path
+      stroke="black"
+      fill="none"
+      d="M23.555,8.729a1.505,1.505,0,0,0-1.406-.98H16.062a.5.5,0,0,1-.472-.334L13.405,1.222a1.5,1.5,0,0,0-2.81,0l-.005.016L8.41,7.415a.5.5,0,0,1-.471.334H1.85A1.5,1.5,0,0,0,.887,10.4l5.184,4.3a.5.5,0,0,1,.155.543L4.048,21.774a1.5,1.5,0,0,0,2.31,1.684l5.346-3.92a.5.5,0,0,1,.591,0l5.344,3.919a1.5,1.5,0,0,0,2.312-1.683l-2.178-6.535a.5.5,0,0,1,.155-.543l5.194-4.306A1.5,1.5,0,0,0,23.555,8.729Z"
+    />
+  ),
+})
+
+export const FavStar: React.FC<Props> = ({ isFav, onClick }) => {
+  return isFav ? (
+    <StarIcon variant="outline" color="yellow.400" onClick={() => onClick()} />
+  ) : (
+    <OutlineStarIcon variant="outline" onClick={() => onClick()} />
+  )
+}
