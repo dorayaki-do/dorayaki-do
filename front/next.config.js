a7aeb36318d02edf8c9433040b992abab93b5f45
GitHayato <gabayt0_0@icloud.com>
2021-09-15 15:43:06 +0900

    production url

--- front/next.config.js
+++ front/next.config.js
@@ -1,6 +1,6 @@
 const isDev = process.env.NODE_ENV === "development";
 module.exports = {
   env: {
-    server: isDev ? "http://localhost" : "https://production-server.com"
+    server: isDev ? "http://localhost" : "http://13.230.60.179"
   },
 }
